e3264b9763a12ce219d84a59fe89a49c5358b0bb
sorouv <sorouv2024@gmail.com>
2025-12-03 12:11:12 +0600

    update js

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
     // Data: at least 6 cards
     const services = [
       {id:1,
-         icon: '<img src="B12-A5-Emergency-Hotline/emergency.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/emergency.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'National Emergency Number',
         eng:'National Emergency',
         number:'999',
         category:'All'
       },
       {id:2,
-       icon: '<img src="B12-A5-Emergency-Hotline/police.png" alt="Police Icon" style="width:15px;height:12px;">',
+       icon: '<img src="B12-A5-Emergency-Hotline/assets/police.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Police Helpline Number',
         eng:'Police',
         number:'999',
         category:'Police'
       },
       {id:3,
-         icon: '<img src="B12-A5-Emergency-Hotline/fire-service.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/fire-service.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Fire Service Number',
         eng:'Fire Service',
         number:'999',
         category:'Fire'
       },
       {id:4,
-       icon: '<img src="B12-A5-Emergency-Hotline/ambulance.png" alt="Police Icon" style="width:15px;height:12px;">',
+       icon: '<img src="B12-A5-Emergency-Hotline/assets/ambulance.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Ambulance Service',
         eng:'Ambulance',
         number:'1994-999999',
         category:'Health'
       },
       {id:5,
-         icon: '<img src="B12-A5-Emergency-Hotline/emergency.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/emergency.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Women & Child Helpline',
         eng:'Women & Child Helpline',
         number:'109',
         category:'Help'
       },
       {id:6,
-         icon: '<img src="B12-A5-Emergency-Hotline/emergency.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/emergency.png" alt="Police Icon" style="width:30px;height:30px;">',
       name:'Anti-Corruption Helpline',
       eng:'Anti-Corruption',
       number:'106',
       category:'Govt'
     },
       {id:7,
-         icon: '<img src="B12-A5-Emergency-Hotline/emergency.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/emergency.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Electricity Helpline',
         eng:'Electricity Outage',
         number:'16216',
         category:'Electricity'
       },
       {id:8,
-         icon: '<img src="B12-A5-Emergency-Hotline/brac.png" alt="Police Icon" style="width:15px;height:12px;">',
+         icon: '<img src="B12-A5-Emergency-Hotline/assets/brac.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Brac Helpline',
         eng:'Brac',
         number:'16445',
         category:'NGO'
       },
       {id:9,
-        icon: '<img src="B12-A5-Emergency-Hotline/Bangladesh-Railway.png" alt="Police Icon" style="width:15px;height:12px;">',
+        icon: '<img src="B12-A5-Emergency-Hotline/assets/Bangladesh-Railway.png" alt="Police Icon" style="width:30px;height:30px;">',
         name:'Bangladesh Railway Helpline',
         eng:'Railway',
         number:'163',
@@ -87,7 +87,7 @@
         card.dataset.id = s.id;
         card.innerHTML = `
           <div class="icon">${s.icon}</div>
-          <button class="heart" aria-label="like"> <img src="B12-A5-Emergency-Hotline/heart.png" alt="heart Icon" style="width:24px;height:24px;"></button>
+          <button class="heart" aria-label="like">  <img src="./B12-A5-Emergency-Hotline/assets/heart.png" alt="" style="width:24px;height:24px;"></button>
           <h3>${s.name}</h3>
           <p class="subtitle">${s.eng}</p>
           <div class="number">${s.number}</div>
@@ -115,6 +115,7 @@
       if(heartBtn && card){
         heartCount++;
         updateCounters();
+
         // small feedback
         heartBtn.style.transform = 'scale(1.12)';
         setTimeout(()=>heartBtn.style.transform='scale(1)',200);
